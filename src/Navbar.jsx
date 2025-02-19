d5c34e1bdc485d630c1eaa5bc83b765b40c54705
Priyansh Dimri <priyanshd0908@gmail.com>
2025-02-19 21:24:30 +0530

    remove comment from navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,6 @@
 function Navbar({ currScore, maxScore, restartGame }) {
   return (
     <div id="navbar">
-      {/* Display reset game button using resetGame function and
-  displays main logo, curr and max score on navbar */}
       <div className="game-title">Memory Card Game</div>
       <div className="nav-tile curr-score">Current Score: {currScore}</div>
       <div className="nav-tile max-score">Max Score: {maxScore}</div>
